5190d862e387525d688637c5e0ce7d5fb8640029
Tuna Han Bulanık <tunahanbulanik@outlook.com>
2024-07-16 21:08:19 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuna Han Bulanık. Kendimi geliştirmek ve bulunduğum sektörü değiştirmek için Patikadayım!");
+        }
+    }
+}
